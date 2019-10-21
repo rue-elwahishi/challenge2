4f81c08e6e12a604768fd9de032ea3a09da5f68d
rue-elwahishi <rubawahishi@gmail.com>
2019-10-21 12:04:18 +0100

    newcommit

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ function each(coll, f) {
 
 function filter(array, predicate) {
   var acc = [];
-  each(array, function(element, i) {
-    if (predicate(element, i)) {
+  each(array, function(element, key) {
+    if (predicate(element, key)) {
       acc.push(element);
     }
   });
@@ -42,17 +42,29 @@ Create a factory function called makeComputer that represents computers,
 What different attributes computers may have? 
 Create two computers object from your factory function and save them in one array called computers!
 
-Note: please write one or two lines here describing your solution.
+Note: I think we should make an array of objects first called computer and insert keys and values inside that contain the paramater in the function ofcourse
+and I could call that computer name by inputing its type, color, weight. PS: (forgot about factory functions).
 */
 
+ var computers = [
+ { name : "ASUS", type : "4th generation", graphicsCard : 960, color : black, weight : "30km"}
+ { name2:  "Sony", type : "1234w4", processor : "COREi7", color: "black", weight : "15km" }
+ ]; 
+
+
 function makeComputer(type, color, weight) {
+
+
+  
+
   // TODO: Your code here
 }
 
 // Write a function displayComputer that can be used to display one computer.
+// here its easy we just write return statement mentiong the keys which will be by refrencing by dot notation and make a sentence basically.
 
 function displayComputer(computer) {
-  // TODO: Your code here
+  return "this computer is" + " " + computers.name + "its type" + " " + computers.type + " " + "and color" + " " + computers.color;
 }
 
 //=============================================================================
@@ -66,15 +78,26 @@ and returns an array of all of those strings, but transformed to upper case.
 You can use toUpperCase method to convert a string to upper case. 
 Solve it using the most appropriate helper functions(each,map,filter).
 
-Note: please write one or two lines here describing your solution.
+Note: we need to loop through every index and transform small letters strings to capital letters using toUpperCase.
 
 var strArr = ['hello', 'world', 'whirled', 'peas'];
 uppercaseAll(strArr); ==> [ 'HELLO', 'WORLD', 'WHIRLED', 'PEAS' ]
 */
 
+
+var strArr = ['hello', 'world', 'whirled', 'peas'];
+
+
 function uppercaseAll(arrayOfStrings) {
+  var arr = [];
+  each(arrayOfStrings, function(element){
+    arr.push(toUpperCase(element));
+
+  })
+
+  return arr;
   // TODO: your code here
-}
+}  //.toUpperCase returns touppercase undefined when its built in function????? I don't understand why.
 
 //=============================================================================
 /*                                  Q3                                       */
@@ -112,6 +135,11 @@ var data = [
 ];
 
 function highestPopulation(arrayOfObjects) {
+  var newArr = [];
+  return filter(arrayOfObjects, function(element, key){
+ 
+    return element > 500000000   });
+  return newArr;
   // TODO: your code here
 }
 
@@ -128,12 +156,21 @@ Note: solve it using the most appropriate helper functions(each,map,filter)
 var numsArray = [2, 6, 20, 8, 14];
 halveAll(numsArray); ==> [ 1, 3, 10, 4, 7 ]
 
-Note: please write one or two lines here describing your solution.
+Note:  first we need to declare newArr we're going to use filter or map to return numbers halved by two
 */
 
-function halveAll(numbers) {
-  // your code is here
+
+ function halveAll(numbers) {
+  var arr = [];
+  return map(numbers, function(element, i){
+    return arr.push(element[i] / 2);
+
+  
+
+  })
+  return arr
 }
+  //
 
 //=============================================================================
 /*                                  Q5                                       */
@@ -145,7 +182,7 @@ Solve it using one of the most appropriate helpers functions(each,map,filter).
 
 values({first : 5, second: 'something' , third : 129}) ==> [5, 'something', 129];
 
-Note: please write one or two lines here describing your solution.
+Note: we need to declate new arr, loop throught the objects keys, takes values and push them into array.
 */
 function values(obj) {
   // TODO: your code here
